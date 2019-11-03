cfba5aa7d9c0d27cf0554254f298372ad2043a40
Alain Guillon <zyrass@outlook.fr>
2019-11-03 12:37:14 +0100

    description de main.ts

--- understand-main/src/main.ts
+++ understand-main/src/main.ts
@@ -1,6 +1,18 @@
+/*
+|----------------------------------------------------------------------------------
+| MAIN.TS
+|----------------------------------------------------------------------------------
+| Tout passe par ce fichier afin de pouvoir faire fonctionner nôtre application.
+*/
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+/*
+|----------------------------------------------------------------------------------
+| AppModule correspond à la classe qui est elle exportée du fichier app.module.ts
+|----------------------------------------------------------------------------------
+*/
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +20,11 @@ if (environment.production) {
   enableProdMode();
 }
 
+/*
+|----------------------------------------------------------------------------------
+| plateformBrowserDynamic().bootstrapModule(AppModule)
+| permet de lancer l'application et donc nous passons la classe AppModule
+|----------------------------------------------------------------------------------
+*/
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
