0eb55bea293845e0646cc5ccf2f24524a2a24687
Alain Guillon <zyrass@outlook.fr>
2019-11-03 17:52:41 +0100

    description app.module.ts

--- understand-main/src/app/app.module.ts
+++ understand-main/src/app/app.module.ts
@@ -1,8 +1,29 @@
+/*
+|----------------------------------------------------------------------------------
+| APP.MODULE.TS
+|----------------------------------------------------------------------------------
+| Le fichier app.module.ts permet de lié le fichier app.component.ts
+| Une fois lié, nous exportons à la fin la classe dans le fichier main.ts
+|----------------------------------------------------------------------------------
+*/
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+/*
+|----------------------------------------------------------------------------------
+| Ici j'importe le fichier app.component.ts
+|----------------------------------------------------------------------------------
+*/
 import { AppComponent } from './app.component';
 
+
+/*
+|----------------------------------------------------------------------------------
+| Déclaration d'une décoration ici : @NgModule
+|----------------------------------------------------------------------------------
+*/
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +32,12 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Chargement d'un components ici (1 seul). 
 })
+
+/*
+|----------------------------------------------------------------------------------
+| Ici nous exportons AppModule qui sera lui utilisé dans le fichier main.ts
+|----------------------------------------------------------------------------------
+*/
 export class AppModule { }
