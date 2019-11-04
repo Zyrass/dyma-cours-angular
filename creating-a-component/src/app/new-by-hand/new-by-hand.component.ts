0c15e1c861d6f9f1939ee98ee6c0555045bcab84
Alain Guillon <zyrass@outlook.fr>
2019-11-04 11:15:58 +0100

    add all files of new-by-hand folder

---
+++ creating-a-component/src/app/new-by-hand/new-by-hand.component.ts
@@ -0,0 +1,60 @@
+// Importation de l'interface Component issu d'Angular/core
+import { Component } from '@angular/core';
+
+// Décorative @Component ci-dessou
+@Component({
+
+    selector:       'app-new-by-hand',
+    templateUrl:    './new-by-hand.component.html',
+    styleUrls:      './new-by-hand.component.css'
+
+})
+
+// Exportation de la classe NewByHandComponent
+export class NewByHandComponent {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Component({
+    selector: 'app-new-by-hand',
+    templateUrl: './new-cli.component.html',
+})
+
+export class NewByHandComponent {}
+
+
