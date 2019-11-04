2c3527e1dcb1f61ae07d95c47f1db0e224994fa4
Alain Guillon <zyrass@outlook.fr>
2019-11-04 09:55:53 +0100

    first commit

---
+++ creating-a-component/src/app/new-cli/new-cli.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-cli',
+  templateUrl: './new-cli.component.html',
+  styleUrls: ['./new-cli.component.css']
+})
+
+export class NewCliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
