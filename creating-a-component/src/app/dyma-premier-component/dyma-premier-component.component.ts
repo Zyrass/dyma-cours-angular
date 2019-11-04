42f516c15478a3047dbcfdc1db2849738ece7c9f
Alain Guillon <zyrass@outlook.fr>
2019-11-04 18:46:37 +0100

    update comments

--- creating-a-component/src/app/dyma-premier-component/dyma-premier-component.component.ts
+++ creating-a-component/src/app/dyma-premier-component/dyma-premier-component.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
     templateUrl:    './dyma-premier-component.component.html'
 })
 
-export class DymaPremierComponent {}
+export class DymaPremierComponent {
+
+    // Définition d'une variable vide qui est elle, typé pour forcé son contenu pour un retour de type string. 
+    title: string;
+
+    // Définition d'un constructor.
+    constructor () {
+
+        // Le title sera remplit d'une chaine de caractère (string) afin d'être utilisé via l'interpolation dans le fichier "dyma-premier-component.component.html".
+        this.title = "Dyma et notre premier composant créer à la main";
+
+    }
+
+}
