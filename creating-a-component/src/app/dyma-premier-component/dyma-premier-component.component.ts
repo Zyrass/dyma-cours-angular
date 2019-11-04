ea6881f88831669ea55538cc09d54f23c0bc03ab
Alain Guillon <zyrass@outlook.fr>
2019-11-04 18:27:43 +0100

    add premier-component with dyma

---
+++ creating-a-component/src/app/dyma-premier-component/dyma-premier-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:       'app-dyma-premier-component',
+    templateUrl:    './dyma-premier-component.component.html'
+})
+
+export class DymaPremierComponent {}
