e8297233f4f63dd79a976be8d557e4d5805114eb
Alain Guillon <zyrass@outlook.fr>
2019-11-05 11:29:04 +0100

    update comments

--- options-cli/src/app/app.module.ts
+++ options-cli/src/app/app.module.ts
@@ -2,10 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+/*
+|----------------------------------------------------------------------------------
+| J'importe ici toutes les classes qui ont été générées via le CLI :
+|----------------------------------------------------------------------------------
+*/
+
+/*
+|----------------------------------------------------------------------------------
+|   - Dans le terminal : ng generate component without-options
+|   - Ma classe généré : WithoutOptionsComponent
+|----------------------------------------------------------------------------------
+*/
 import { WithoutOptionsComponent } from './without-options/without-options.component';
+
+/*
+|----------------------------------------------------------------------------------
+|   - Dans le terminal : ng generate component with-flat
+|   - Ma classe généré : WithFlatComponent
+|----------------------------------------------------------------------------------
+*/
 import { WithFlatComponent } from './with-flat.component';
+
+/*
+|----------------------------------------------------------------------------------
+|   - Dans le terminal : ng generate component with-inlinestyle
+|   - Ma classe généré : WithInlineStyleComponent
+|----------------------------------------------------------------------------------
+*/
 import { WithInlineStyleComponent } from './with-inline-style/with-inline-style.component';
+
+/*
+|----------------------------------------------------------------------------------
+|   - Dans le terminal : ng generate component with-inlinetemplate
+|   - Ma classe généré : WithInlineTemplateComponent
+|----------------------------------------------------------------------------------
+*/
 import { WithInlineTemplateComponent } from './with-inline-template/with-inline-template.component';
+
+/*
+|----------------------------------------------------------------------------------
+|   - Dans le terminal : ng generate component without-test-file
+|   - Ma classe généré : WithoutTestFileComponent
+|----------------------------------------------------------------------------------
+*/
+import { WithoutTestFileComponent } from './without-test-file/without-test-file.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +55,8 @@ import { WithInlineTemplateComponent } from './with-inline-template/with-inline-
     WithoutOptionsComponent,
     WithFlatComponent,
     WithInlineStyleComponent,
-    WithInlineTemplateComponent
+    WithInlineTemplateComponent,
+    WithoutTestFileComponent
   ],
   imports: [
     BrowserModule
---
+++ options-cli/src/app/without-test-file/without-test-file.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-without-test-file',
+  templateUrl: './without-test-file.component.html',
+  styleUrls: ['./without-test-file.component.css']
+})
+export class WithoutTestFileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
