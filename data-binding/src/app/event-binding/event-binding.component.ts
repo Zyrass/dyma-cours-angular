b489f839996c1d900a784b2edb4dceae19a252a2
Alain Guillon <zyrass@outlook.fr>
2019-11-08 09:33:41 +0100

    update content and comments of event-binding

--- data-binding/src/app/event-binding/event-binding.component.ts
+++ data-binding/src/app/event-binding/event-binding.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './event-binding.component.html',
   styleUrls: ['./event-binding.component.css']
 })
-export class EventBindingComponent {}
+
+export class EventBindingComponent {
+
+  /*
+  |----------------------------------------------------------------------------------
+  | Définition de ma variable qui sera interpolée
+  |----------------------------------------------------------------------------------
+  */
+  public myBackgroundColor: string = "lightgrey" // Couleur par défaut
+
+  /*
+  |----------------------------------------------------------------------------------
+  | Méthode qui définit le changement de couleur du paragraphe.
+  |----------------------------------------------------------------------------------
+  | Avec 2 paramètres :
+  |   1 - paramColor de type "string" définit la couleur du fond du paragraphe
+  |   2 - event de type "event" permet de connaitre le type utilisé
+  |----------------------------------------------------------------------------------
+  */
+  changeTheBackgroundColor(paramColor: string, event:Event): void {
+
+    this.myBackgroundColor = paramColor;
+    console.log(paramColor + " avec le type event : " + event.type);
+
+  }
+
+}
