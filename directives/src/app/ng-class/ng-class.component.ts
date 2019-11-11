585ca017fa653b46e5c0156df2f42f9ee9ada328
Alain Guillon <zyrass@outlook.fr>
2019-11-11 13:01:36 +0100

    for ng-class : add a external css, add comments

--- directives/src/app/ng-class/ng-class.component.ts
+++ directives/src/app/ng-class/ng-class.component.ts
@@ -3,6 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ng-class',
   templateUrl: './ng-class.component.html',
-  styles: []
+  styleUrls: ['./ng-class.component.css']
 })
-export class NgClassComponent {}
+export class NgClassComponent {
+
+  /*
+  |----------------------------------------------------------------------------------
+  | Définition des propréités qui sont a modifier dans le constructor.
+  |----------------------------------------------------------------------------------
+  */
+  boxShadow: boolean;
+  borderRadius: boolean;
+  borderSolid: boolean;
+
+  constructor() {
+
+    /*
+    |----------------------------------------------------------------------------------
+    | Toutes les propriétés sont initialisé à false afin que rien ne soit activé.
+    |----------------------------------------------------------------------------------
+    */
+    this.boxShadow = false;
+    this.borderRadius = false;
+    this.borderSolid = false;
+
+  }
+
+}
