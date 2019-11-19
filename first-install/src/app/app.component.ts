7fc94133057817c4c90084752221877de2a920d7
Alain Guillon <zyrass@outlook.fr>
2019-11-19 15:20:05 +0100

    update html

--- first-install/src/app/app.component.ts
+++ first-install/src/app/app.component.ts
@@ -4,7 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'first-install';
+  
+  // Pour la navigation + le titre de la page
+  public chapter: number = 1;
+  public lesson: number = 1
+  public title: string = "Première installation"
+
+
+
 }
