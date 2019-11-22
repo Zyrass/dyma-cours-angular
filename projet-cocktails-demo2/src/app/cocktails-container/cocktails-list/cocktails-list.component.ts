570a9348770f26b907eaeaac206149332fc76d6e
Alain Guillon <zyrass@outlook.fr>
2019-11-22 13:40:15 +0100

    update full project with comments and features OK

--- projet-cocktails-demo2/src/app/cocktails-container/cocktails-list/cocktails-list.component.ts
+++ projet-cocktails-demo2/src/app/cocktails-container/cocktails-list/cocktails-list.component.ts
@@ -1,26 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { Cocktail } from '../../shared/cocktail.model';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Cocktail } from 'src/app/shared/cocktail.model';
 
 @Component({
   selector: 'app-cocktails-list',
   templateUrl: './cocktails-list.component.html',
-  styles: []
+  styleUrls: ['./cocktails-list.component.css']
 })
 export class CocktailsListComponent implements OnInit {
 
-  public tableauDesCocktails: Cocktail[] = [];
+  /*
+  |-------------------------------------------------------------------------------------
+  | Je créer avec le décorateur Input une liaison avec le parent : (Parent vers Enfant)
+  | (cocktails-container.component.ts)
+  |-------------------------------------------------------------------------------------
+  */
+  @Input() public listingCocktails: Cocktail[];
 
-  constructor() {
-    this.tableauDesCocktails = [
-      new Cocktail("Mojito", "url", "alt", "description"),
-      new Cocktail("Virgin Colada", "url", "alt", "description"),
-      new Cocktail("Secret de la mer", "url", "alt", "description"),
-      new Cocktail("Monkey Jame Sour", "url", "alt", "description"),
-    ];
+  /*
+  |-------------------------------------------------------------------------------------
+  | Je créer avec le décorateur Output une liaison avec le parent : (Enfant vers Parent)
+  | (cocktails-container.component.ts)
+  | Je définis ici un EventEmitter de type nombre qui permettra de devenir l'index de
+  | notre itération en cours.
+  |-------------------------------------------------------------------------------------
+  */
+  @Output() public indexEvent: EventEmitter<number> = new EventEmitter<number>();
+
+  /*
+  |-------------------------------------------------------------------------------------
+  | Je définis ici une méthode ou je passe en paramètre un nombre.
+  | Ce paramètre sera l'event créer précédemment.
+  | La méthode emit() provient de la classe EventEmitter.
+  |-------------------------------------------------------------------------------------
+  */
+  getCocktail(parametreIndex: number): void {
+    this.indexEvent.emit(parametreIndex);
   }
 
-  ngOnInit() {
-
-  }
-
+  constructor() {}
+  ngOnInit() {}
 }
